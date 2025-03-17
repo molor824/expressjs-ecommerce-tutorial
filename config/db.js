2a344ca6811d587e2f2224c22a65894a70d5f66a
molor824 <molormargaderdene@gmail.com>
2025-03-17 22:32:02 +0800

    fix: formatting

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 export async function connectDB() {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URL);
-        console.log(`MongoDB conntected: ${conn.connection.host}`);
-    } catch (err) {
-        console.error(err);
-        process.exit(-1);
-    }
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MongoDB conntected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(-1);
+  }
 }
--- index.js
+++ index.js
@@ -2,31 +2,34 @@ import express from "express";
 import products from "./data/products.js";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import User from "./models/user.js";
 
 dotenv.config();
 connectDB();
 const port = process.env.PORT;
 if (!port) {
-    throw new Error("No port");
+  throw new Error("No port");
 }
 const app = express();
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello world!");
+  res.send("Hello world!");
 });
 app.get("/api/products", (req, res) => {
-    res.json(products);
+  res.json(products);
 });
 app.get("/api/products/:productId", (req, res) => {
-    const { productId } = req.params;
-    const product = products.find((p) => p.id === parseInt(productId));
-    if (!product) {
-        res.sendStatus(404);
-    } else {
-        res.json(product);
-    }
+  const { productId } = req.params;
+  const product = products.find((p) => p.id === parseInt(productId));
+  if (!product) {
+    res.sendStatus(404);
+  } else {
+    res.json(product);
+  }
 });
 
-app.listen(port, () => console.log(`Server listening at port http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Server listening at port http://localhost:${port}`)
+);
